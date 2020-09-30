f1edb5d0a5c74622cae94d8e42a4623fd7ede3c4
Phil <phil@PhildeMac-mini.engenius.login>
2020-09-30 18:33:12 +0800

    First commit.

---
+++ IRImagePickerController-swift/IRImagePickerController_swift.h
@@ -0,0 +1,19 @@
+//
+//  IRImagePickerController_swift.h
+//  IRImagePickerController-swift
+//
+//  Created by Phil on 2020/9/30.
+//  Copyright © 2020 Phil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRImagePickerController_swift.
+FOUNDATION_EXPORT double IRImagePickerController_swiftVersionNumber;
+
+//! Project version string for IRImagePickerController_swift.
+FOUNDATION_EXPORT const unsigned char IRImagePickerController_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRImagePickerController_swift/PublicHeader.h>
+
+
